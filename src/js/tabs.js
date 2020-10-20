3d45be0adfe32dca36f34e20262fbb6864fb5e76
danya0 <samarkin.danil@yandex.ru>
2020-10-20 19:44:32 +0300

    improved the structure of the code, tabs work

--- src/js/tabs.js
+++ src/js/tabs.js
@@ -4,25 +4,29 @@ const tabs = (headerSelector, tabSelector, contentSelector, activeClass) => {
         content = document.querySelectorAll(contentSelector);
 
     let indexActiveTab = 0;
+    let aviableNow = true;
 
     // * Функиця которая скрывает таб
     function hideTabContent(prevIndex, nextIndex) {
         if (prevIndex === nextIndex) {
             return;
         }
+        setTimeout(() => {
+            if(indexActiveTab !== prevIndex) {
+                content[prevIndex].style.display = 'none';
+            }
+        }, 300);
         content.forEach(item => {
-            item.style.zIndex = 0;
+            item.style.zIndex = -1;
+            item.style.position = 'absolute';
             item.classList.remove(activeClass);
         })
-        setTimeout(() => {
-            content[prevIndex].style.display = 'none';
-        }, 300);
     }
     
     // * Функиця которая показывает конкретный таб
     function showTabContent(i) {
-        content[i].style.position = 'static';
         content[i].style.display = 'block';
+        content[i].style.position = 'static';
         content[i].style.zIndex = 3;
         setTimeout(() => {
             content[i].classList.add(activeClass);
@@ -38,13 +42,15 @@ const tabs = (headerSelector, tabSelector, contentSelector, activeClass) => {
         const target = e.target;
         if(target.classList.contains(tabSelector.replace(/\./, '')) || 
         target.parentNode.classList.contains(tabSelector.replace(/\./, ''))) {
-            tabs.forEach((item, i)=> {
-                if(target == item || target.parentNode == item) {
-                    hideTabContent(indexActiveTab, i)
-                    showTabContent(i);
-                    indexActiveTab = i;
-                }
-            });
+            if (aviableNow) {
+                tabs.forEach((item, i)=> {
+                    if(target == item || target.parentNode == item) {
+                        hideTabContent(indexActiveTab, i)
+                        showTabContent(i);
+                        indexActiveTab = i;
+                    }
+                });
+            }
         }
 
     });
